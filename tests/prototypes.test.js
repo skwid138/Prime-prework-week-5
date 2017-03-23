831ef6fe5eab41f77ac84af8dfd7c6a298f8a12f
Huck Brock <huckbrock@gmail.com>
2017-03-23 16:51:38 -0500

    tests working

--- tests/prototypes.test.js
+++ tests/prototypes.test.js
@@ -1,6 +1,3 @@
-
-var logSpy = sinon.spy(console, "log");
-
 describe('basemode prototype answers', function() {
   // somethingFun exists, name somthing and fun w/type Cat
   it('animal somethingFun exists', function() {
@@ -12,43 +9,63 @@ describe('basemode prototype answers', function() {
 
   // call meow functtion
   it('somethingFun should have a meow function that logs meow', function() {
+    var logSpy = sinon.spy(console, "log");
+
     somethingFun.meow(); // doesn't return, but logs
     expect(logSpy).to.be.called;
     expect(logSpy.callCount).to.equal(1);
     expect(logSpy.args[0][0]).to.equal('meow');
-    logSpy.reset();
+
+    logSpy.restore();
   });
 
 });
 
-// describe('basemode prototype answers', function() {
-//   // var logSpy = sinon.spy(console, "log");
-//   it('should log the name outside of anyFunction', function() {
-//     somethingFun.meow(); // doesn't return, but logs
-//     expect(logSpy).to.be.called;
-//     expect(logSpy.callCount).to.equal(1);
-//     expect(logSpy.args[0][0]).to.equal('meow');
-//     logSpy.reset();
-//   });
-//
-// });
+describe('basemode prototype answers', function() {
+
+  it('should log the firstName and lastName properties of an animal object in 2 separate logs', function() {
+    var logSpy = sinon.spy(console, "log");
+    var daniel = {
+      firstName : "Daniel",
+      lastName : "Striped",
+      type : "Tiger"
+    }
+
+    animalFirstandLastName(daniel); // doesn't return, but logs
+    expect(logSpy).to.be.called;
+    expect(logSpy.callCount).to.equal(2);
+    expect(logSpy.args[0][0]).to.equal('Daniel');
+    expect(logSpy.args[1][0]).to.equal('Striped');
+
+    logSpy.restore();
+  });
+
+
+});
 
 describe('hardmode prototype answers', function() {
+
   // call isHappy true
-  it('should have an isHappy function that logs purrrrrr when given true', function() {
+  it('should have an isHappy function that logs "purr" when given true', function() {
+    var logSpy = sinon.spy(console, "log");
+
     somethingFun.isHappy(true); // call function with true
     expect(logSpy).to.be.called;
     expect(logSpy.callCount).to.equal(1);
-    expect(logSpy.args[0][0]).to.equal('purrrrrr');
-    logSpy.reset();
+    expect(logSpy.args[0][0]).to.equal('purr');
+
+    logSpy.restore();
   });
 
   // call isHappy false
   it('should have an isHappy function that logs crabby face when given false', function() {
+    var logSpy = sinon.spy(console, "log");
+
     somethingFun.isHappy(false); // call function with false
     expect(logSpy).to.be.called;
     expect(logSpy.callCount).to.equal(1);
     expect(logSpy.args[0][0]).to.equal('crabby face');
+
     logSpy.restore();
   });
 });
