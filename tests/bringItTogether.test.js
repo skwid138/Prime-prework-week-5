96d1c1678216b82ffcf142ec4a7a8ce126492060
Huck Brock <huckbrock@gmail.com>
2017-03-23 17:04:13 -0500

    Update bringItTogether.test.js

--- tests/bringItTogether.test.js
+++ tests/bringItTogether.test.js
@@ -1,35 +1,32 @@
-var chai = require('chai');
-var rooms = require('../assignment/bringItTogether');
-
 describe('promode Testing the Classroom Builder Application', function() {
 
   describe('the Room constructor function', function() {
 
     describe('returned object has a property called: capacity', function() {
       it('should have a property capacity', function() {
-        var room = new rooms.Room([2, 1, 1, 'red']);
+        var room = new Room([2, 1, 1, 'red']);
         chai.expect(room).to.have.ownProperty('capacity');
       });
 
       it('capacity should match the number given in array[0]', function() {
-        var room = new rooms.Room([2, 1, 1, 'red']);
+        var room = new Room([2, 1, 1, 'red']);
         chai.expect(room.capacity).to.be.equal(2);
       });
     });
 
     describe('returned object has a property called: rollingChairs', function() {
       it('should have a property rollingChairs', function() {
-        var room = new rooms.Room([2, 1, 1, 'blue']);
+        var room = new Room([2, 1, 1, 'blue']);
         chai.expect(room).to.have.ownProperty('rollingChairs');
       });
 
       it('rollingChairs should be an array', function() {
-        var room = new rooms.Room([6, 2, 4, 'red']);
+        var room = new Room([6, 2, 4, 'red']);
         chai.expect(room.rollingChairs).to.be.instanceof(Array);
       });
 
       it('rollingChairs length should match given array[1]', function() {
-        var room = new rooms.Room([6, 2, 4, 'red']);
+        var room = new Room([6, 2, 4, 'red']);
         chai.expect(room.rollingChairs).to.have.lengthOf(2);
       });
 
@@ -37,17 +34,17 @@ describe('promode Testing the Classroom Builder Application', function() {
 
     describe('returned object has a property called: cubes', function() {
       it('should have a property cube', function() {
-        var room = new rooms.Room([2, 1, 1, 'blue']);
+        var room = new Room([2, 1, 1, 'blue']);
         chai.expect(room).to.have.ownProperty('cubes');
       });
 
       it('cubes should be an array', function() {
-        var room = new rooms.Room([6, 2, 4, 'red']);
+        var room = new Room([6, 2, 4, 'red']);
         chai.expect(room.cubes).to.be.instanceof(Array);
       });
 
       it('cubes length should match given array[2]', function() {
-        var room = new rooms.Room([6, 2, 4, 'red']);
+        var room = new Room([6, 2, 4, 'red']);
         chai.expect(room.cubes).to.have.lengthOf(4);
       });
 
@@ -68,27 +65,29 @@ describe('promode Testing the Classroom Builder Application', function() {
   // after(function() {
   //   constructorSpy.restore();
   // });
-  //
-  // describe('the createBooksWithConstructor function', function() {
+
+  // describe('the createRoomsWithConstructor function', function() {
+  //   var  constructorSpy = sinon.spy(Room);
   //   it('should call the Book constructor 3 times', function() {
-  //     var books = objects.createBooksWithConstructor();
+  //     var books = convertRooms();
   //
   //     // sinon spy counts the number of times the constructor was called
-  //     chai.expect(constructorSpy.calledThrice).to.be.true;
+  //     chai.expect(constructorSpy.called).to.be.true;
   //   });
   //
-  //   it('should return 3 objects in an array', function() {
-  //     var books = objects.createBooksWithConstructor();
-  //     chai.expect(books).to.have.lengthOf(3);
-  //   });
+  //   // it('should return 3 objects in an array', function() {
+  //   //   var books = convertRooms();
+  //   //   chai.expect(books).to.have.lengthOf(3);
+  //   // });
   //
-  //   it('should return book objects with 4 specific properties', function() {
-  //     var books = objects.createBooksWithConstructor();
-  //     chai.expect(books[0]).to.have.ownProperty('title');
-  //     chai.expect(books[0]).to.have.ownProperty('author');
-  //     chai.expect(books[0]).to.have.ownProperty('pageCount');
-  //     chai.expect(books[0]).to.have.ownProperty('haveRead');
-  //   });
+  //   // it('should return book objects with 4 specific properties', function() {
+  //   //   var books = convertRooms();
+  //   //   chai.expect(books[0]).to.have.ownProperty('title');
+  //   //   chai.expect(books[0]).to.have.ownProperty('author');
+  //   //   chai.expect(books[0]).to.have.ownProperty('pageCount');
+  //   //   chai.expect(books[0]).to.have.ownProperty('haveRead');
+  //   // });
+  //   constructorSpy.reset();
   // });
 
 });
