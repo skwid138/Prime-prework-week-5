7b9efa4a8baaa3ef88247972f2265ddaab2e72f5
Antoinette Smith <antoinette@primeacademy.io>
2017-03-24 09:12:50 -0500

    resolved merge

--- assignment/bringItTogether.js
+++ assignment/bringItTogether.js
@@ -1,49 +1,4 @@
-
-// Modeled after Tier 2 Bonus Calculator challenge. Convert arrays of rooms to Room objects
-convertRooms();
-
-function convertRooms() {
-  // [seats needed, rollingChairs, cubes, color of seats]
-  var roomsArray = [[20, 14, 12, 'red'], [24, 10, 40, 'blue'], [23, 18, 30, 'black'], [18, 13, 13, 'blue']];
-
-  for(var i = 0; i < roomsArray.length; i++) {
-    var room = new Room(roomsArray[i]);
-    console.log('new room: ', room);
-  }
-
-}
-
-/**
- * Represents a classroom at Prime
- * @constructor
- * @param {Array} array - a single room array of data
-**/
-function Room(array) {
-  this.capacity = array[0];
-  this.rollingChairs = [];
-  // array[1] is a our number of chairs
-  for(var j = 0; j < array[1]; j++) {
-    this.rollingChairs.push(new RollingChair('rolling', array[3]));
-  }
-  this.cubes = [];
-  // array[2] is a number of cubes
-  for(j = 0; j < array[2]; j++) {
-    this.cubes.push(new Cube(24));
-    // console.log('cube vol: ', this.cubes[j].volume());
-  }
-}
-
-/**
- * Represents a Rolling Chair seat
- * @constructor
- * @param {String} type - type of seat this is
- * @param {String} color - color of the seat fabric
-**/
-function RollingChair(type, color) {
-  this.type = type;
-  this.color = color;
-}
-
+// 1 - Create properties and methods for the Cube constructor
 /**
  * Represents a Cube seat
  * @constructor
@@ -51,30 +6,78 @@ function RollingChair(type, color) {
  * @property {Function} volume - method that calculates the volume of the cube
 **/
 function Cube(length) {
+  // 1a - create a length property that is equal to the provided argument, length
   this.length = length;
+  // 1b - Create a volume method that returns the volume of the cube
+  // The volume of a cube is the length to the third power
+  // PRO TIP: Consider using Math.pow, a built-in JavaScript method to calculate this
   this.volume = function() {
     return Math.pow(this.length, 3);
   };
 }
 
+// 2 - Create properties and methods for the Rolling Chair constructor
+/**
+ * Represents a Rolling Chair seat
+ * @constructor
+ * @param {String} type - type of seat this is
+ * @param {String} color - color of the seat fabric
+**/
+function RollingChair(type, color) {
+  // 2a - Create a type property that is equal to the provided argument, type
+  this.type = type;
+  // 2b - Create a color property that is equal to the provided argument, color
+  this.color = color;
+}
+
+// 3 - Create properties and methods for the Room constructor
 /**
  * Represents a classroom at Prime
  * @constructor
- * @param {Array} roomArray - an array of data representing a single room
+ * @param {Number} capacity - the room's capacity
  *  @property {Number} capacity - number of seats in this room
  *  @property {Array} rollingChairs - an array of rolling chair objects
  *  @property {Array} cubes - an array of cube objects
- *  @property {Array} color - color of rollingChairs
 **/
 
+function Room(capacity) {
+  // 3a - Create a capacity property that is equal to the provided array's first index
+  this.capacity = capacity;
+  // 3b - Create a rollingChairs property that is equal to an empty array
+  this.rollingChairs = [];
+  // 3b - Create a cubes property that is equal to an empty array
+  this.cubes = [];
+}
 
+// 4 - The following function should create a Room object from an array of room data
+// and return the created room
+function convertRooms(roomAsArray) {
+  // 4a - Create a room object using value for the capacity
+  // 4b - Create another loop that adds new rolling chair objects with type "rolling"
+  // and the color provided in the array to the created room's rollingChairs array
+  // 4c - Create yet another loop that adds new cube objects with a length of 24 to
+  // the created room's cubes array
+  // 4d - return the room object
+  var room = new Room(roomAsArray[0]);
 
+  for(var j = 0; j < roomAsArray[1]; j++) {
+    room.rollingChairs.push(new RollingChair('rolling', roomAsArray[3]));
+  }
 
+  for(var k = 0; k < roomAsArray[2]; k++) {
+    room.cubes.push(new Cube(24));
+  }
 
-// -- DON'T TOUCH CODE BELOW --
-// module.exports = {
-//   Room: Room,
-//   convertRooms: convertRooms,
-//   RollingChair: RollingChair,
-//   Cube: Cube
-// };
+  return room;
+
+}
+
+// For each inner array we have [capacity, rollingChairs, cubes, color of seats]
+var roomsArray = [[20, 14, 12, 'red'], [24, 10, 40, 'blue'], [23, 18, 30, 'black'], [18, 13, 13, 'blue']];
+
+// 5 - Convert each room array to a room object
+// 5a - Loop through the roomsArray
+// In the body of the loop, call the convertRooms function for each room array
+for(var i = 0; i < roomsArray.length; i++) {
+  convertRooms(roomsArray[i]);
+}
