b638fb2a475523513b14f3a5bc4bf22d7cf20b39
Huck Brock <huckbrock@gmail.com>
2017-03-24 13:43:39 -0500

    new bringittogether assignment structure

--- assignment/bringItTogether.js
+++ assignment/bringItTogether.js
@@ -1,15 +1,4 @@
-console.log('File loaded!');
-
-/**
- * Represents a Rolling Chair seat
- * @constructor
- * @param {String} type - type of seat this is
- * @param {String} color - color of the seat fabric
-**/
-function RollingChair(type, color) {
-  // Write constructor logic here to create a single rolling chair object
-}
-
+// 1 - Create properties and methods for the Cube constructor
 /**
  * Represents a Cube seat
  * @constructor
@@ -17,36 +6,89 @@ function RollingChair(type, color) {
  * @property {Function} volume - method that calculates the volume of the cube
 **/
 function Cube(length) {
-  // Write constructor logic here to create a single cube seat object
+  // 1a - create a length property that is equal to the provided argument, length
+
+  // 1b - Create a volume method that returns the volume of the cube
+  // The volume of a cube is the length to the third power
+  // PRO TIP: Consider using Math.pow, a built-in JavaScript method to calculate this
+
+
+
 }
 
+// 2 - Create properties and methods for the Rolling Chair constructor
+/**
+ * Represents a Rolling Chair seat
+ * @constructor
+ * @param {String} type - type of seat this is
+ * @param {String} color - color of the seat fabric
+**/
+function RollingChair(type, color) {
+  // 2a - Create a type property that is equal to the provided argument, type
 
+
+  // 2b - Create a color property that is equal to the provided argument, color
+
+
+}
+
+// 3 - Create properties and methods for the Room constructor
 /**
  * Represents a classroom at Prime
  * @constructor
- * @param {Array} roomArray - an array of data representing a single room
+ * @param {Number} capacity - the room's capacity
  *  @property {Number} capacity - number of seats in this room
  *  @property {Array} rollingChairs - an array of rolling chair objects
  *  @property {Array} cubes - an array of cube objects
- *  @property {Array} color - color of rollingChairs
 **/
-function Room(roomArray) {
-  // Write constructor logic here to convert the array into a Room object.
-  // You will need to also create RollingChair and Cube objects!
+
+function Room(capacity) {
+  // 3a - Create a capacity property that is equal to the provided array's first index
+
+
+  // 3b - Create a rollingChairs property that is equal to an empty array
+
+
+  // 3b - Create a cubes property that is equal to an empty array
+
+
 }
 
+// 4 - The following function should create a Room object from an array of room data
+// and return the created room
+function convertRooms(roomAsArray) {
+  // 4a - Create a room object using value for the capacity
+  // 4b - Create another loop that adds new rolling chair objects with type "rolling"
+  // and the color provided in the array to the created room's rollingChairs array
+  // 4c - Create yet another loop that adds new cube objects with a length of 24 to
+  // the created room's cubes array
+  // 4d - return the room object
 
 
-// Call the function defined below to actually do something!
-convertRooms();
 
-function convertRooms() {
-  // Each array of data is in the order of: [total seats needed, rollingChairs, cubes, color of seats]
-  var roomsArray = [[20, 10, 10, 'red'], [24, 10, 14, 'blue'], [23, 8, 11, 'black'], [18, 5, 13, 'blue']];
 
-  console.log(roomsArray);
 
-  // Convert each room array into a Room object
-  // Write code here, remember to log the new object you create at the end of
-  // each iteration.
+
+
+
+
 }
+
+// For each inner array we have [capacity, rollingChairs, cubes, color of seats]
+var roomsArray = [[20, 14, 12, 'red'], [24, 10, 40, 'blue'], [23, 18, 30, 'black'], [18, 13, 13, 'blue']];
+
+// 5 - Convert each room array to a room object
+// 5a - Loop through the roomsArray
+// In the body of the loop, call the convertRooms function for each room array
+// store all the converted rooms objects in convertedRoomsArray
+function convertRoomsList(arrayOfRooms){
+  var convertedRoomsArray = [];
+
+
+
+  //return convertedRoomsArray to make it available outside this function:
+  return convertedRoomsArray;
+}
+
+//Call convertRoomsList
+convertRoomsList(roomsArray);
