e1a29593766057da0415f406b9b2a9355ac38a1f
Huck Brock <huckbrock@gmail.com>
2017-03-24 13:41:38 -0500

    merged news tests

--- tests/bringItTogether.test.js
+++ tests/bringItTogether.test.js
@@ -4,56 +4,127 @@ describe('promode Testing the Classroom Builder Application', function() {
 
     describe('returned object has a property called: capacity', function() {
       it('should have a property capacity', function() {
-        var room = new Room([2, 1, 1, 'red']);
+        var room = convertRooms([2, 1, 1, 'red']);
         chai.expect(room).to.have.ownProperty('capacity');
       });
 
-      it('capacity should match the number given in array[0]', function() {
-        var room = new Room([2, 1, 1, 'red']);
+      it('capacity should match the number given in constructor', function() {
+        var room = new Room(2);
         chai.expect(room.capacity).to.be.equal(2);
       });
     });
 
+
     describe('returned object has a property called: rollingChairs', function() {
       it('should have a property rollingChairs', function() {
-        var room = new Room([2, 1, 1, 'blue']);
+        var room = convertRooms([2, 1, 1, 'blue']);
         chai.expect(room).to.have.ownProperty('rollingChairs');
       });
 
-      it('rollingChairs should be an array', function() {
-        var room = new Room([6, 2, 4, 'red']);
+      it('rollingChairs should be an array of objects', function() {
+        var room = convertRooms([10, 3, 7, 'neon']);
         chai.expect(room.rollingChairs).to.be.instanceof(Array);
+        chai.expect(room.rollingChairs[0]).to.be.instanceof(Object);
       });
 
       it('rollingChairs length should match given array[1]', function() {
-        var room = new Room([6, 2, 4, 'red']);
-        chai.expect(room.rollingChairs).to.have.lengthOf(2);
+        var room = convertRooms([8, 4, 4, 'maroon']);
+        chai.expect(room.rollingChairs).to.have.lengthOf(4);
       });
 
     });
 
     describe('returned object has a property called: cubes', function() {
       it('should have a property cube', function() {
-        var room = new Room([2, 1, 1, 'blue']);
+        var room = convertRooms([2, 1, 1, 'blue']);
         chai.expect(room).to.have.ownProperty('cubes');
       });
 
-      it('cubes should be an array', function() {
-        var room = new Room([6, 2, 4, 'red']);
+      it('cubes should be an array of objects', function() {
+        var room = convertRooms([6, 2, 4, 'red']);
         chai.expect(room.cubes).to.be.instanceof(Array);
+        chai.expect(room.cubes[0]).to.be.instanceof(Object);
       });
 
       it('cubes length should match given array[2]', function() {
-        var room = new Room([6, 2, 4, 'red']);
-        chai.expect(room.cubes).to.have.lengthOf(4);
+        var room = convertRooms([30, 13, 17, 'firetruck red']);
+        chai.expect(room.cubes).to.have.lengthOf(17);
       });
+
 
     });
 
   }); // end Room constructor tests
 
+
+  describe('the Cube constructor function', function() {
+    describe('returned object has a property called: length', function() {
+      it('should have a property "length"', function() {
+        var cube = new Cube(18);
+        chai.expect(cube).to.have.ownProperty('length');
+      });
+
+      it('should have a method "volume" that returns the volume', function() {
+        var cube = new Cube(4);
+        //Does it have the volume method:
+        expect(cube).to.respondTo('volume');
+
+        //Does volume method work:
+        chai.expect(cube.volume()).to.be.equal(64);
+
+      });
+    });
+
+  });
+
+  describe('the RollingChair constructor function', function() {
+    describe('returned object has a property called: color', function() {
+      it('should have a string property of type', function() {
+        var chair = new RollingChair('rolling', 'yellow');
+        chai.expect(chair).to.have.ownProperty('type');
+        chai.expect(chair.type).to.be.equal('rolling');
+      });
+
+      it('should have a string property of color', function() {
+        var chair = new RollingChair('rolling', 'yellow');
+        chai.expect(chair).to.have.ownProperty('type');
+        chai.expect(chair.color).to.be.equal('yellow');
+      });
+    });
+
+  });
+
+  describe('the room array converter', function() {
+
+    it('should return an array of Objects', function() {
+      var newRooms = convertRoomsList(roomsArray);
+      chai.expect(newRooms).to.be.instanceof(Array);
+      chai.expect(newRooms[0]).to.be.instanceof(Object);
+      chai.expect(newRooms[1]).to.be.instanceof(Object);
+    });
+
+    it('should return array with length that matches given rooms array', function() {
+      var testArray = [[21, 13, 13, 'orange'], [24, 10, 40, 'green'], [25, 10, 55, 'purplish orange']];
+      var newRooms = convertRoomsList(testArray);
+      chai.expect(newRooms).to.have.lengthOf(4);
+    });
+
+    // it('should call the convertRooms function', function() {
+    //   var spy = sinon.spy(convertRooms);
+    //   convertRoomsList(roomsArray);
+    //   // chai.expect(room.cubes).to.be.instanceof(Array);
+    //   // assert(spy.callCount).to.be.equal.equal(4)
+    //   expect(spy).to.be.called;
+    // });
+
+    // });
+  });
+
+
+
+
   /**
-    Set up a function spy using Sinon. This will count how many times the Book constructor is calledThrice
+  Set up a function spy using Sinon. This will count how many times the Book constructor is calledThrice
   **/
   // var sinon = require('sinon');
   // var constructorSpy;
--- tests/prototypes.test.js
+++ tests/prototypes.test.js
@@ -1,13 +1,11 @@
-
 describe('basemode prototype answers', function() {
   // somethingFun exists, name somthing and fun w/type Cat
   it('animal somethingFun exists', function() {
     expect(somethingFun).to.exist;
-    expect(somethingFun.firstName).to.equal('Somthing');
+    expect(somethingFun.firstName).to.equal('Something');
     expect(somethingFun.lastName).to.equal('Fun');
     expect(somethingFun.type).to.equal('Cat');
   });
-
 
   // call meow functtion
   it('somethingFun should have a meow function that logs meow', function() {
@@ -21,53 +19,49 @@ describe('basemode prototype answers', function() {
     logSpy.restore();
   });
 
-  describe('hardmode prototype answers', function() {
+  it('should log the firstName and lastName properties of an animal object in 2 separate logs', function() {
+    var logSpy = sinon.spy(console, "log");
+    var daniel = {
+      firstName : "Daniel",
+      lastName : "Striped",
+      type : "Tiger"
+    }
 
-    describe('basemode prototype answers', function() {
+    animalFirstandLastName(daniel); // doesn't return, but logs
+    expect(logSpy).to.be.called;
+    expect(logSpy.callCount).to.equal(2);
+    expect(logSpy.args[0][0]).to.equal('Daniel');
+    expect(logSpy.args[1][0]).to.equal('Striped');
 
-      it('should log the firstName and lastName properties of an animal object in 2 separate logs', function() {
-        var logSpy = sinon.spy(console, "log");
-        var daniel = {
-          firstName : "Daniel",
-          lastName : "Striped",
-          type : "Tiger"
-        }
-
-        animalFirstandLastName(daniel); // doesn't return, but logs
-        expect(logSpy).to.be.called;
-        expect(logSpy.callCount).to.equal(2);
-        expect(logSpy.args[0][0]).to.equal('Daniel');
-        expect(logSpy.args[1][0]).to.equal('Striped');
-
-        logSpy.restore();
-      });
+    logSpy.restore();
+  });
 
 
-    });
+});
 
-    describe('hardmode prototype answers', function() {
+describe('hardmode prototype answers', function() {
 
-      // call isHappy true
-      it('should have an isHappy function that logs "purr" when given true', function() {
-        var logSpy = sinon.spy(console, "log");
+  // call isHappy true
+  it('should have an isHappy function that logs "purr" when given true', function() {
+    var logSpy = sinon.spy(console, "log");
 
-        somethingFun.isHappy(true); // call function with true
-        expect(logSpy).to.be.called;
-        expect(logSpy.callCount).to.equal(1);
-        expect(logSpy.args[0][0]).to.equal('purr');
+    somethingFun.isHappy(true); // call function with true
+    expect(logSpy).to.be.called;
+    expect(logSpy.callCount).to.equal(1);
+    expect(logSpy.args[0][0]).to.equal('purr');
 
-        logSpy.restore();
-      });
+    logSpy.restore();
+  });
 
-      // call isHappy false
-      it('should have an isHappy function that logs crabby face when given false', function() {
-        var logSpy = sinon.spy(console, "log");
+  // call isHappy false
+  it('should have an isHappy function that logs crabby face when given false', function() {
+    var logSpy = sinon.spy(console, "log");
 
-        somethingFun.isHappy(false); // call function with false
-        expect(logSpy).to.be.called;
-        expect(logSpy.callCount).to.equal(1);
-        expect(logSpy.args[0][0]).to.equal('crabby face');
+    somethingFun.isHappy(false); // call function with false
+    expect(logSpy).to.be.called;
+    expect(logSpy.callCount).to.equal(1);
+    expect(logSpy.args[0][0]).to.equal('crabby face');
 
-        logSpy.restore();
-
-      });
+    logSpy.restore();
+  });
+});
