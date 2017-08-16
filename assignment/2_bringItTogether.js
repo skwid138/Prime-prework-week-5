914cfbc3395bbc8c9043f21cff8f24d2ea459e25
skwid138 <skwid138@gmail.com>
2017-08-16 13:55:23 -0500

    worked on some pro mode stuff

--- assignment/2_bringItTogether.js
+++ assignment/2_bringItTogether.js
@@ -1,3 +1,4 @@
+console.log('sourced 2_bringItTogether.js');
 /**
  * This entire file is a web application. Nearly everything in this file relies on
  * other functions in and variables also within this file.
@@ -26,15 +27,21 @@
 **/
 function Cube(length) {
   // 1a - create a length property that is equal to the provided argument, length
-
+  //console.log('start Cube');
+  this.length = length;
+  //console.log(length);
   // 1b - Create a volume method that returns the volume of the cube
   // PRO TIP: A "method" is a function that is assigned to an object's property
   // The volume of a cube is the length to the third power
   // PRO TIP: Consider using Math.pow, a built-in JavaScript method to calculate this
-
-
-
+  this.volume = function() {
+    //console.log('start volume');
+    return Math.pow(this.length, 3);
+  };
 }
+
+// var fourSquare = new Cube(4);
+// console.log(fourSquare.volume());
 
 // 2 - Create properties and methods for the Rolling Chair constructor
 /**
@@ -45,12 +52,15 @@ function Cube(length) {
 **/
 function RollingChair(type, color) {
   // 2a - Create a type property that is equal to the provided argument, type
-
+  this.type = type;
 
   // 2b - Create a color property that is equal to the provided argument, color
-
+this.color = color;
 
 }
+
+// var chair = new RollingChair('adjustable', 'red');
+// console.log(chair);
 
 // 3 - Create properties and methods for the Room constructor
 /**
@@ -64,13 +74,13 @@ function RollingChair(type, color) {
 
 function Room(capacity) {
   // 3a - Create a capacity property that is equal to the provided capacity argument
-
+  this.capacity = capacity;
 
   // 3b - Create a rollingChairs property that is equal to an empty array
-
+  this.rollingChairs = [];
 
   // 3c - Create a cubes property that is equal to an empty array
-
+  this.cubes = [];
 
 }
 
@@ -79,21 +89,25 @@ function Room(capacity) {
 function convertRooms(roomAsArray) {
   // 4a - Create a room object with the Room constructor you created above
   // and pass into the constructor the value for the capacity from room array.
+  //for (var t = 0; t < roomAsArray.length; t ++) {
+     var room = new Room(roomAsArray[0]);
+  //}
   // 4b - Create a loop that adds new rolling chair objects with type "rolling"
   // and the color provided in the array to the created room's rollingChairs array
+  for (var i = 0; i < roomAsArray[1]; i ++) {
+    room.rollingChairs[i] = new RollingChair('rolling', roomAsArray[3]);
+  }
   // 4c - Create yet another loop that adds new cube objects with a length of 24 to
   // the created room's cubes array
+  for (var j = 0; j < roomAsArray[2]; j ++) {
+    room.cubes[j] = new Cube(24);
+  }
   // 4d - return the room object
-
-
-
-
-
-
-
-
-
+  return room;
 }
+
+// var test = convertRooms([5, 10, 15, 'yellow']);
+// console.log(test);
 
 // For each inner array we have [capacity, rollingChairs, cubes, color of seats]
 var roomsArray = [[20, 14, 12, 'red'], [24, 10, 40, 'blue'], [23, 18, 30, 'black'], [18, 13, 13, 'blue']];
@@ -104,12 +118,13 @@ var roomsArray = [[20, 14, 12, 'red'], [24, 10, 40, 'blue'], [23, 18, 30, 'black
 // store all the converted rooms objects in convertedRoomsArray
 function convertRoomsList(arrayOfRooms){
   var convertedRoomsArray = [];
-
-
-
+  for (var i = 0; i < arrayOfRooms.length; i ++) {
+    convertRooms(arrayOfRooms[i]);
+  }
   //return convertedRoomsArray to make it available outside this function:
   return convertedRoomsArray;
 }
 
 //Call convertRoomsList
 convertRoomsList(roomsArray);
+//console.log(convertRoomsArray);
